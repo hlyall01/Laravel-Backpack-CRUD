18072ee2c02963c8c4547cc789a4fdc8ef926d78
hlyall01 <evil_harry16@hotmail.com>
2023-03-31 12:48:04 +1100

    Update CrudController.php

--- src/app/Http/Controllers/CrudController.php
+++ src/app/Http/Controllers/CrudController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Backpack\CRUD\app\Http\Controllers;
+namespace hlyall01\Backpack\CRUD\app\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
